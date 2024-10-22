36352fa6462f3944cce46a2ba84fdb742fee02a7
Michael Chang <michaelxchang.ca@gmail.com>
2024-10-22 16:57:11 -0700

    more file path fixes

--- scripts/imageScrolling.js
+++ scripts/imageScrolling.js
@@ -1,13 +1,13 @@
 // Define image data with src, alt, and text
 const imageData = [
-  { src: "/images/drag.png", alt: "", text: "", link: "" },
-  { src: "/images/illustration/Knight2.png", alt: "Knight", text: "Knight", link: "/collections/knights.html" },
-  { src: "/images/animation/HD.gif", alt: "Helldivers", text: "Helldivers", link: "/collections/helldivers.html" },
-  { src: "/images/illustration/Elk_small.png", alt: "Elk", text: "Elk", link: "" },
-  { src: "/images/illustration/Sushi.png", alt: "Sushi", text: "Sushi", link: "" },
-  { src: "/images/animation/Coolvibes.gif", alt: "Cool Vibes", text: "Cool Vibes", link: "" },
-  { src: "/images/animation/explo.gif", alt: "Explosion", text: "Explosion", link: "" },
-  { src: "/images/animation/TowerAnimation.gif", alt: "Tower", text: "Tower", link: "" }
+  { src: "images/drag.png", alt: "", text: "", link: "" },
+  { src: "images/illustration/Knight2.png", alt: "Knight", text: "Knight", link: "collections/knights.html" },
+  { src: "images/animation/HD.gif", alt: "Helldivers", text: "Helldivers", link: "collections/helldivers.html" },
+  { src: "images/illustration/Elk_small.png", alt: "Elk", text: "Elk", link: "" },
+  { src: "images/illustration/Sushi.png", alt: "Sushi", text: "Sushi", link: "" },
+  { src: "images/animation/Coolvibes.gif", alt: "Cool Vibes", text: "Cool Vibes", link: "" },
+  { src: "images/animation/explo.gif", alt: "Explosion", text: "Explosion", link: "" },
+  { src: "images/animation/TowerAnimation.gif", alt: "Tower", text: "Tower", link: "" }
 ];
 
 
