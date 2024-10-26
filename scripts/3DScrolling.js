2856d8ad72fab11fbcb9acfd10c6ad13de446d65
Michael Chang <michaelxchang.ca@gmail.com>
2024-10-26 14:13:45 -0700

    Shoji

--- scripts/3DScrolling.js
+++ scripts/3DScrolling.js
@@ -4,6 +4,7 @@ const imageData = [
   { src: "images/3Drenders/Sign_1.png", alt: "Wooden Sign", text: "Wooden Sign - Blender & Substance 3D", link: "" },
   { src: "images/3Drenders/Shield_1.png", alt: "Shield", text: "Shield - Blender & Substance 3D", link: "" },
   { src: "images/3Drenders/Torch_1.png", alt: "Torch", text: "Torch - Blender & Substance 3D", link: "" },
+  { src: "images/3Drenders/Shoji_1.png", alt: "Torch", text: "Stylized Shoji - Blender & Substance 3D", link: "" },
 
 ];
 
