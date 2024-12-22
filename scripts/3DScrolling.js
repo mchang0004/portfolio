1c737a9915fa9a6b2fdb529af51d874889ac289e
Michael Chang <michaelxchang.ca@gmail.com>
2024-12-21 20:00:01 -0800

    table_1

--- scripts/3DScrolling.js
+++ scripts/3DScrolling.js
@@ -6,7 +6,7 @@ const imageData = [
   { src: "images/3Drenders/Torch_1.png", alt: "Torch", text: "Torch - Blender & Substance 3D", link: "" },
   { src: "images/3Drenders/Shoji_1.png", alt: "Torch", text: "Stylized Shoji - Blender & Substance 3D", link: "" },
   { src: "images/3Drenders/Pillar_1.png", alt: "Torch", text: "Midpoly Pillar - Blender & Substance 3D", link: "" },
-
+  { src: "images/3Drenders/Table_1.png", alt: "Torch", text: "Table With Cloth - Blender & Substance 3D", link: "" },
 ];
 
 
